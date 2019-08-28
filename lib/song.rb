d9549ae1aa32fd6d78b2454279af072c473dae85
Jharell Chambers <jharell.chambers@selu.edu>
2019-08-28 01:28:10 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -22,6 +22,11 @@ class Song
     return song 
   end
   
-  def self.create_by_name
+  def self.create_by_name(song_name)
+    song = self.new
+    song.name = song_name
+    @@all << self 
+    return song 
+  end
 
 end
