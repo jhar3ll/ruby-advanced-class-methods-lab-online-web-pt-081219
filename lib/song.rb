74101d9c659f4549e40348760390e09e780806a5
Jharell Chambers <jharell.chambers@selu.edu>
2019-08-28 01:46:29 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -36,7 +36,7 @@ class Song
   def self.find_or_create_by_name(song_name)
     song = song_name
     
-    if @@all.include?(song_name)
+    if self.all.include?(song_name)
       self.find_by_name(song_name)
       return song 
     else 
